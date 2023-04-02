03efa4475d060f6aa70144ba8028e2c526fc5632
KRABn <128624304+KRABn@users.noreply.github.com>
2023-04-02 18:46:49 +0300

    Update AWMSPORT.js

--- js/AWMSPORT.js
+++ js/AWMSPORT.js
@@ -3,7 +3,7 @@ window.onload = function () {
         backgroundColor: "#00000000",
         title: {
             text: "AWM Sport" ,
-            fontColor: "rgb(170, 84, 255)",
+            fontColor: "rgb(247, 237, 252)",
         },
         exportEnabled: true,
   zoomEnabled: true,
